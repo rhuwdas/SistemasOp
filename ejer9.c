246d14cb8a2f65b6ca5a7f6f10907d482d01fd2e
rhuwdas <chimiemmanuel@gmail.com>
2026-03-11 08:02:23 -0600

    hasta el 9

--- ejer9.c
+++ ejer9.c
@@ -8,7 +8,9 @@
 
 
 int main (int argc, char *argv[]) {
-        /* ./ejer7 EPEP AJSD*/
+
+        system("clear");
+
         int i;
 
         printf("Ejecutando el programa invocador (Ejercicio 7). Sus argumentos son:\n");
@@ -17,11 +19,27 @@ int main (int argc, char *argv[]) {
 
                 printf("argv[%d]: %s\n", i, argv[i]);
 
-        sleep(2);
+        sleep(5);
 
-        strcpy(argv[0], "PEPE PICA PAPAS CON UN PICO");
+        char *argv2[6];
 
-        if (execv ("./ejer8", argv) < 0) {
+        char arg[5][20];
+
+        for (i = 0; i < 5; i++){
+
+                printf ("Argumento [%d]: ",i);
+
+                scanf ("%s",arg[i]);
+
+        }
+
+        strcpy(argv2[0], "./ejemplo8");
+
+        for (i=0; i<5; i++)
+
+                argv2[i+1]=arg[i];
+
+        if (execv ("./ejemplo8", argv2) < 0) {
 
                 printf("Error en la invocacion del Ejercicio-8\n");
 
